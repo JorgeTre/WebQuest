58fd03ea8ee3ba79367a8cfb1e8a1e0245e9a0a1
JorgeTre <42651808+JorgeTre@users.noreply.github.com>
2019-04-14 21:25:04 -0600

    WebQuest
    cuestioanrio de control de versiones

---
+++ WebQuest./WebQuest.kt
@@ -0,0 +1,50 @@
+/*
+
+1.¿ Que es un control de versiones?
+Es un sistema que registra los cambios realizados sobre un 
+archivo o conjunto de archivos a lo largo del tiempo, de modo 
+que puedas recuperar versiones específicas más adelante.
+
+2.¿Cuáles son las ventajas de utilizar un controlador de versiones?
+- Puedes volver a cualquier punto del desarrollo para ver qué aspecto tenía 
+un determinado fichero de código, o volver a una versión donde todo funcionaba 
+antes de haber metido la pata.
+
+- Puedes trabajar en distintas características de forma simultánea, guardando 
+los cambios en cada una de ellas, y uniéndolos al desarrollo principal si ya 
+han sido lo suficientemente probadas.
+
+- Puedes echar un vistazo para ver quién realizó un determinado cambio, y cuándo lo hizo.
+
+3.¿Cuáles son los controladores de versiones disponibles para android studio?
+- Bitbucket
+- GitHub
+- CVS
+- Git
+- Google Cloud
+- Mercurial
+- Subversion
+
+4.¿Cuáles son los servidores de controladores de versiones mas conocidos?
+CVS, Subversion, Git, Mercurial, y Bazaar
+
+5.Investigue los siguientes términos Push, Commit, Branch, Delete, Stack y Repositorio.
+
+Push: cuando alguien del equipo hace un cambio en el repositorio nos permite enviar los cambios al servidor.
+
+Commit: añadir un cambio o un fichero a un repositorio debemos pasar esos cambios a stage antes de hacer los cambios (commit) y enviarlos (push)  al servidor. Realizar un cambio en el proyecto. Formalmente, almacenar un cambio en la base de datos del control de versiones de forma que pueda ser incorporado en lanzamientos futuros del proyecto. "Commit" puede ser utilizado como un verbo o como un sustantivo. Como un sustantivo, es esencialmente un sinónimo de "cambio".
+
+Branch: Es una copia del proyecto, bajo el control de versiones, pero aislado, de forma que los cambios realizados en esta rama no afecten al resto del proyecto y vice versa, excepto cuando los cambios sean deliberadamente "unidos" de un lado al otro. Las ramas también son conocidas como "lineas de desarrollo".
+
+Delete: elimina algunos ficheros de la lista de la configuración.
+
+Stack: es un desarrollador con un perfil técnico muy completo que conoce bien tanto lo referente a back-end como lo referente a front-end, se maneja en sistemas y sabe entender. Se trata de un perfil cada vez más demandado y bien remunerado.
+
+Repositorio: El repositorio es el lugar en el que se almacenan los datos actualizados e históricos de cambios, a menudo en un servidor. Una base de datos en la que los cambios son almacenados. Algunas versiones de sistemas de control de versiones son centralizados, es decir, existe un único repositorio maestro, el cual almacena todos los cambios en el proyecto.
+
+6.PD:Crear un GIST en su controlador de versiones preferido y almacenar el 
+documento con las respuestas a este WebQuest. Luego publicar su gist en el
+foro de la materia correspondiente.
+
+
+ */
